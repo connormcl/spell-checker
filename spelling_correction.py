2b9ee81f7611c681a35e32f55601e687b6d49e1c
Connor McLaughlin <connor.mclaughlin@yale.edu>
2016-02-29 17:24:45 -0500

    Adds comments describing modifications made to the Levenshtein distance

--- spelling_correction.py
+++ spelling_correction.py
@@ -1,6 +1,16 @@
 # Connor McLaughlin
 #
 # Simple spell checker that uses a slightly modified version of the Levenshtein distance
+#
+# Modifications:
+#
+# i.  The first improvement I made was to increase the cost of inserting or deleting a letter that’s at the beginning or the end of a word. (I increased this cost to 2.) The logic behind this is that the program originally had a tendency to delete (valid) prefixes/suffixes of words in order to match shorter suggestions. This proved problematic because the beginnings and the ends of words are typically pretty close to what they should be.
+# ii. The second improvement I made was to lower the cost of substituting “a” for “e” and vice versa. This helped because those letters seemed to be easily misplaced.
+# iii.    The third improvement I made was to lower the cost of substituting “s” for “c” and vice versa for the same reason as (ii).
+# iv. The fourth improvement I made was to increase the cost of deleting “c” because that seemed to be a correct letter in most types of words.
+# v.  The fifth improvement was the same as (iv) except with the letter “v”.
+# vi. The sixth improvement was to increase the deletion cost of a letter at the beginning or end of a word from 2 to 4. This is more costly than inserting at the beginning of a word because letters on either side of a word seem to be more often right than wrong.
+# vii.    A change I expected would help but didn’t was lowering the cost of substituting “s” for a vowel (and vice versa). “S” is a very common letter, so I figured a common mistake would be accidentally switching the place of “s” and a vowel.
 
 import sys
 
@@ -64,7 +74,7 @@ def min_distance(target, source):
     return distance[n][m]
 
 
-print 'Writing corrections to suggestions.txt, this may take a couple minutes...'
+print 'Writing corrections to suggestions.txt, this may take a few minutes...'
 for word in misspellings:
     abs_min = min_distance(dictionary[0], word)
     correction = dictionary[0]
